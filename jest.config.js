58e6cae63970a2f23faefcaba90d12e2ac70a6ac
Gago <xeroice@gmail.com>
2022-05-13 16:01:56 -0400

    fixing deprecation warning for testURL -> testEnvironmentOptions.url

--- jest.config.js
+++ jest.config.js
@@ -3,19 +3,21 @@ process.env.TZ = 'UTC';
 module.exports = {
   globals: {
     'ts-jest': {
-      disableSourceMapSupport: true
-    }
+      disableSourceMapSupport: true,
+    },
   },
   verbose: true,
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testEnvironmentOptions: {
+    url: 'http://localhost',
   },
   preset: 'ts-jest',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testURL: 'http://localhost',
   transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
   testMatch: ['**/*.spec.ts'],
   collectCoverage: true,
   collectCoverageFrom: ['src/*.ts', '!./index.ts'],
-  coverageDirectory: './coverage/'
+  coverageDirectory: './coverage/',
 };
