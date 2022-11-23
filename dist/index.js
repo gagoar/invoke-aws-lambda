6441b5112f047c1d275927ee0e47d8659b3d1cb9
Github Workflow <action@github.com>
2022-11-23 22:58:15 +0000

    Changes found on Workflow

--- dist/index.js
+++ dist/index.js
@@ -1,27 +1,10 @@
 'use strict';
 var __create = Object.create;
 var __defProp = Object.defineProperty;
-var __defProps = Object.defineProperties;
 var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
-var __getOwnPropDescs = Object.getOwnPropertyDescriptors;
 var __getOwnPropNames = Object.getOwnPropertyNames;
-var __getOwnPropSymbols = Object.getOwnPropertySymbols;
 var __getProtoOf = Object.getPrototypeOf;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
-var __propIsEnum = Object.prototype.propertyIsEnumerable;
-var __defNormalProp = (obj, key, value) =>
-  key in obj
-    ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value })
-    : (obj[key] = value);
-var __spreadValues = (a, b) => {
-  for (var prop in b || (b = {})) if (__hasOwnProp.call(b, prop)) __defNormalProp(a, prop, b[prop]);
-  if (__getOwnPropSymbols)
-    for (var prop of __getOwnPropSymbols(b)) {
-      if (__propIsEnum.call(b, prop)) __defNormalProp(a, prop, b[prop]);
-    }
-  return a;
-};
-var __spreadProps = (a, b) => __defProps(a, __getOwnPropDescs(b));
 var __commonJS = (cb, mod) =>
   function __require() {
     return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
@@ -21907,7 +21890,7 @@ var setAWSCredentials = () => {
 var getParams = () => {
   return Object.keys(Props).reduce((memo, prop) => {
     const value = (0, import_core.getInput)(prop);
-    return value ? __spreadProps(__spreadValues({}, memo), { [prop]: value }) : memo;
+    return value ? { ...memo, [prop]: value } : memo;
   }, {});
 };
 var setAWSConfigOptions = () => {
